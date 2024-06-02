8bee3536ef1df09a9d0d5de0dc45d2d5ab3aac2b
Yann <iamyanndias@gmail.com>
2024-06-01 22:09:51 -0300

    refactor: rename directory, remove hello endpoint, register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,20 @@ from typing import Any, Dict, Optional
 
 from flask import Flask
 
+from app.register_blueprints import register_blueprints
+
 
 def run(test_config: Optional[Dict[str, Any]] = None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=str(Path(app.instance_path) / 'yuta.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=str(Path(app.instance_path) / "yuta.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -25,9 +27,6 @@ def run(test_config: Optional[Dict[str, Any]] = None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello() -> str:
-        return 'Hello, World!'
+    register_blueprints(app)
 
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
